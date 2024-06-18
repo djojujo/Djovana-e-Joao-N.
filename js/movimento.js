19dd92940e4dd755bbfafffb12f560a528537736
djojujo <159132703+djojujo@users.noreply.github.com>
2024-06-18 14:35:05 -0300

    .

--- js/movimento.js
+++ js/movimento.js
@@ -1,25 +1,3 @@
-/*const track = document.querySelector('.carousel-inner');
-const items = document.querySelectorAll('.slide');
-
-let index = 0;
-const totalItems = items.length;
-
-function nextSlide(){
-  index++;
-  if (index >= totalItems){
-    index = 0;
-  }
-  updateSlide();
-}
-
-function updateSlide() {
-  const itemWidth = items[0].clientWidth;
-  track.style.transform = `translateX(-${index * itemWidth}px)`;
-}
-
-setInterval(nextSlide, 3000); // Troca de slide a cada 3 segundos (3000 milissegundos)
-*/
-
 const track = document.querySelector('.carousel-inner');
 const items = document.querySelectorAll('.slide');
 
