3f9a22adc71a45f7199bf942c85febc2003a6db6
djojujo <159132703+djojujo@users.noreply.github.com>
2024-06-18 14:29:58 -0300

    .

--- js/movimento.js
+++ js/movimento.js
@@ -1,4 +1,4 @@
-const track = document.querySelector('.carousel-inner');
+/*const track = document.querySelector('.carousel-inner');
 const items = document.querySelectorAll('.slide');
 
 let index = 0;
@@ -18,10 +18,10 @@ function updateSlide() {
 }
 
 setInterval(nextSlide, 3000); // Troca de slide a cada 3 segundos (3000 milissegundos)
+*/
 
-
-/*const abacaxi = document.querySelector('carousel-inner');
-const items = document.querySelectorAll('slide');
+const abacaxi = document.querySelector('.carousel-inner');
+const items = document.querySelectorAll('.slide');
 
 let index = 0;
 const totalitems = items.length;
@@ -39,4 +39,4 @@ function updateslide(){
     abacaxi.style.transform = 'translateX(-${index * itemWidth}px)';
 
 }
-setInterval(nextslide, 3000);*/
+setInterval(nextslide, 3000);
