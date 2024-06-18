4715afbd791ce4db918ed98b9a6e649b4de194c7
djojujo <159132703+djojujo@users.noreply.github.com>
2024-06-18 14:12:53 -0300

    ;

--- js/movimento.js
+++ js/movimento.js
@@ -1,4 +1,4 @@
-const track = document.querySelector('carousel-inner');
+const abacaxi = document.querySelector('carousel-inner');
 const items = document.querySelectorAll('slide');
 
 let index = 0;
@@ -14,7 +14,7 @@ function nextslide(){
 
 function updateslide(){
     const itemwitdth = items[0].clientWidth;
-    track.style.transform = 'translateX(-${index * itemWidth}px)';
+    abacaxi.style.transform = 'translateX(-${index * itemWidth}px)';
 
 }
 setInterval(nextslide, 3000);
